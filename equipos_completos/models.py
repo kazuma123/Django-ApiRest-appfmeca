6c1225b6d3e8c3c914055e47257e4b34e23757d1
Harrigan159 <echambir@unsa.edu.pe>
2022-06-21 11:57:16 -0500

    make it better2345

--- equipos_completos/models.py
+++ equipos_completos/models.py
@@ -3,22 +3,18 @@ from django.db import models
 
 # Create your models here.
 class EquiposCompletos(models.Model):
+
+    #area = models.CharField(max_length=255)
+    tag = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=255)
+    codigo = models.CharField(max_length=255)
     machine_type = models.CharField(max_length=255)
-    area = models.CharField(max_length=255)
-    tag_equipo = models.CharField(max_length=255)
-    equipo_principal = models.CharField(max_length=255)
-    sistema = models.CharField(max_length=255)
-    sub_sistema = models.CharField(max_length=255)
-    lazo_control = models.CharField(max_length=255)
-    tag_componente = models.CharField(max_length=255)
-    componente = models.CharField(max_length=255)
-    ubicacion_tecnica = models.CharField(max_length=255)
-    numero_equipo = models.CharField(max_length=255)
-    descripcion_equipo = models.TextField()
-    criticidad = models.CharField(max_length=255)
+    descripcion = models.TextField()
     funcion_equipo = models.TextField()
     falla_funcional = models.TextField()
-    componente_pauta = models.CharField(max_length=255)
+    ds_modo_falla = models.CharField(max_length=255)
+    ds_efecto_modo_falla = models.CharField(max_length=255)
+    descripcion_tarea = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
